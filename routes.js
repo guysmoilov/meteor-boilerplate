2acb75290d308c0567c080ed5848e23ace9350a5
Guy Smoilov <guy.smoilov@gmail.com>
2015-03-27 21:53:18 +0300

    Added login page and routes with authentication

--- routes.js
+++ routes.js
@@ -1,16 +1,20 @@
 var subs = new SubsManager();
 
 Router.plugin('auth', {
-  authenticate: 'home',
-  authorize: {
-    allow: function() {
-      return false;
-    }
-  },
-  except: ['home']
+  authenticate: 'login',
+  except: ['login'],
 });
 
 Router.route('/', {
+  template: 'login',
+  name: 'login',
+  noAuth: {
+    home: 'home'
+  },
+  onBeforeAction: ['noAuth'],
+});
+
+Router.route('/home', {
   template: 'home',
   name: 'home'
 });
